e9e8a0aeeafbe39e9e79111f6ae243867e6cd26a
samis <samis@sami77>
2025-10-11 10:52:15 +0530

    added excel utility and database utility

---
+++ Sales_Inventory/src/test/java/packagetest/TestingUtility.java
@@ -0,0 +1,19 @@
+package packagetest;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+
+import genericUtility.ExcelUtility;
+
+
+public class TestingUtility {
+
+	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+		System.out.println("start");
+		ExcelUtility eu = new ExcelUtility();
+		String data = eu.getDataFromExcel("Sheet1", 1, 1);
+		System.out.println(data);
+//		eu.setDataBackToExcel("Sheet1", 5, 5, "sami");
+	}
+}
