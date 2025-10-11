f90ff62ecff7d9be82540435611e1acf25b25220
samis <samis@sami77>
2025-10-11 12:00:06 +0530

    added

--- Sales_Inventory/src/test/java/packagetest/A.java
+++ Sales_Inventory/src/test/java/packagetest/A.java
@@ -13,6 +13,7 @@ public  void m1() throws EncryptedDocumentException, IOException {
 	ExcelUtility eu = new ExcelUtility();
 	String data = eu.getDataFromExcel("Sheet1", 1, 1);
 	System.out.println(data);
-	System.out.println("Added for feature branch");
+	System.out.println("Added for feature branch-1");
+	System.out.println("added for feature branch-2");
 }
 }
