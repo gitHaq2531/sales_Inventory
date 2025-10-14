2b26a6c432684d01be7e565d78081dc38f7dd5a6
Lenovo <Lenovo@DESKTOP-AD0OTFU>
2025-10-14 11:31:27 +0530

    commit

--- Sales_Inventory/src/main/java/genericUtility/WebDriverUtility.java
+++ Sales_Inventory/src/main/java/genericUtility/WebDriverUtility.java
@@ -1,9 +1,19 @@
 package genericUtility;
 
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebDriverUtility {
+	/**
+	 * @author Mugdha
+	 */
 
 	public void select(WebElement element, int index) {
 		Select sel = new Select(element);
@@ -17,5 +27,28 @@ public class WebDriverUtility {
 		Select sel = new Select(element);
 		sel.selectByVisibleText(visibletext);
 	}
+	
+	
+	
+	public void waitforpageload(WebDriver driver) {
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(15));
+		wait.until(ExpectedConditions.alertIsPresent());
+		
+	}
+	
+	public void handleAlert(WebDriver driver) {
+		try {
+		waitforpageload(driver);
+		Alert alt=driver.switchTo().alert();
+		alt.accept();
+		System.out.println("Alert accepted successfully.");
+		}catch (Exception e) {
+			System.out.println("Alert accepted successfully.");
+			
+		}
+	}
+	
+	
 
+	
 }
